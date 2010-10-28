4c118e75917dd66384907f90e477fa8df3d3fd4d
Brady Bonnette <brady.bonnette@gmail.com>
2010-10-27 20:28:38 -0400

    First inital commit, barebones Rails creation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interview::Application.config.secret_token = '633e426166006ca4e7a5afc0273bccf8dc5dd1155c86f204a045f6c5a4c83a349979d0337cb08284c4990377c9eba9891890a2b8c69b53de4f8fa22d7cd8bcae'
